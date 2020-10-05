367a04d9827c5e228cf587922c288b4f19f943e0
Vladimir Lazarchuk <northwoodzp@gmail.com>
2020-10-05 23:01:22 +0300

    spinner created, animation improved, interceptor extended

---
+++ src/app/services/event-handler.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EventHandlerService {
+    public isSpinnerDisplayed: BehaviorSubject<boolean> = new BehaviorSubject(false);
+    public isErrorDisplayed: BehaviorSubject<boolean> = new BehaviorSubject(false);
+    public errorMessage: BehaviorSubject<string> = new BehaviorSubject('');
+
+    public showSpinner(): void {
+        this.isSpinnerDisplayed.next(true);
+    }
+
+    public hideSpinner(): void {
+        this.isSpinnerDisplayed.next(false);
+    }
+
+    public showError(message: string): void {
+        this.isErrorDisplayed.next(true);
+        this.errorMessage.next(message);
+    }
+
+    public hideError(): void {
+        this.errorMessage.next('');
+        this.isErrorDisplayed.next(false);
+    }
+
+    public handleError(e: any): Observable<any> {
+        console.log(e);
+        this.showError(e.error.message);
+        return throwError(e);
+    }
+
+}
